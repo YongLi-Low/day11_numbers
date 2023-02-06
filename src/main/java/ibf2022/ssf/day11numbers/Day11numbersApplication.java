2be72dd0cc09e29e25f3de989db2a4ad90ceea6d
Yongli <yl.low91@gmail.com>
2023-02-06 14:52:52 +0800

    first commit

---
+++ src/main/java/ibf2022/ssf/day11numbers/Day11numbersApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.ssf.day11numbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11numbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11numbersApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ibf2022/ssf/day11numbers/controllers/ImageController.java
@@ -0,0 +1,53 @@
+package ibf2022.ssf.day11numbers.controllers;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+// Tell Spring Boot this is the controller
+@Controller
+@RequestMapping(path = {"/", "/index.html"})
+public class ImageController {
+    
+    public String getFileNames() {
+
+        // First generate the list of file names to a list
+        List<String> result = new ArrayList<>();
+        File[] files = new File("src/main/resources/static/images").listFiles();
+
+        for (File file: files) {
+            if (file.isFile()) {
+                result.add(file.getName());
+            }
+        }
+        // System.out.println(result.toString());
+
+        //Get the size of the list
+        Integer size = result.size();
+
+        Random rand = new Random();
+
+        Integer fileIndex = rand.nextInt(size);
+
+        // return the randomly generated file name
+        return result.get(fileIndex);
+    }
+
+    // GET '/' or '/index.html'
+    @GetMapping
+    public String getImage(Model model) {
+
+        String fileName = getFileNames();
+        System.out.printf("The file is %s\n", fileName);
+        model.addAttribute("number", "/images/" + fileName);
+
+        return "number";
+    }
+    
+}
